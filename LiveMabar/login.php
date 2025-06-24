9b02928e0e203aecb941d4e3c3f69ae0ebf074d8
nikita74939 <itanikita20005@gmail.com>
2025-06-24 10:10:37 +0700

    mengubah tampilan login 10% progress

--- LiveMabar/login.php
+++ LiveMabar/login.php
@@ -4,26 +4,38 @@
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charset="utf-8" />
+    <!-- style -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css"/>
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300..700&display=swap"
+        rel="stylesheet">
+    <!-- icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
   </head>
   <body>
-    <div class="login-container">
-      <div class="login-wrapper">
-        <div class="login-content">
-          <img class="login-wallpaper" src="../LiveMabar/wallpaper-fanny.jpg" />
-          <div class="login-overlay"></div>
-          <div class="login-title">WELCOME</div>
-          <p class="login-description">Want to take control of the queue and see your game stats? Let’s log in!</p>
-          <a href="// di isi url tiktok auth =http://localhost/html/LiveMabar/tiktok-callback.php">
-            <button class="login-button">
-              <div class="login-button-text">Login with TikTok</div>
-            </button>
-          </a>
-          <img class="login-icon" src="" />
+    <div class="container m-0 p-0">
+      <div class="row m-0 p-0">
+        <div class="col m-0 p-0" id="pict">
+          <img src="img/Wallpaper fanny.jpeg" class="img-fluid" alt="fanny">
+        </div>
+        <div class="col" id="login-text">
+
         </div>
       </div>
     </div>
+    
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="script.js"></script>
   </body>
 </html>
